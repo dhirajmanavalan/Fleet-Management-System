29c386182cb1585f6f6e77fa4d1d9ce1354a1006
Dhirajkumar <kumardhiraj7720@gmail.com>
2026-01-05 10:25:49 +0530

    given security to seating capacity

--- EcoRideMain.py
+++ EcoRideMain.py
@@ -47,7 +47,7 @@ class Vehicle:
 class ElectricCar(Vehicle):
     def __init__(self, vehicle_id, model, battery_percentage, seating_capacity):
         super().__init__(vehicle_id, model, battery_percentage)
-        self.seating_capacity = seating_capacity
+        self.__seating_capacity = seating_capacity
 
     @property
     def seating_capacity(self):
